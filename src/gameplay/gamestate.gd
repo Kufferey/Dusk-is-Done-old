3d7a1803e80fad29eaeba3450ae55e787187237c
Kufferey <109652222+Kufferey@users.noreply.github.com>
2024-01-19 12:45:27 -0500

    Add files via upload

--- src/gameplay/gamestate.gd
+++ src/gameplay/gamestate.gd
@@ -8,8 +8,8 @@ var cherriesContainer:Node3D
 var cherryTypes:Array[String] = ['normal', 'spoiled']
 var cherryTypesStory:Array[String]
 
-var currentHeldCherry:Node3D
-var currentHoveredCherry:Node3D
+var currentHeldItem:Node3D
+var currentHoveredItem:Node3D
 
 var cherrySection:int
 
@@ -22,7 +22,10 @@ var isInCherryCombineState:bool
 
 var playerCamera:Node3D
 var playerRaycast:RayCast3D
+
 var playerItemHolderHand:Node3D
+
+# Gameplay Vars
 
 func _create_cherry_on_bush(typeOfCherry:String, positionObject:Vector3, rotationObject:Vector3, amount:int) -> void:
 	if amount >= 5:
@@ -52,6 +55,19 @@ func _remove_all_cherries() -> void:
 	
 	print(totalCount, " Objects Deleted.")
 
+func _create_object_from_prefab(pathToPrefab:String, objectPosition:Vector3, objectRotation:Vector3) -> void:
+	var prefabFullPath:String = ("res://scenes/prefab/" + pathToPrefab)
+	if prefabFullPath:
+		if prefabFullPath == null:
+			print("Object could not be created. No pathToPrefab argument.")
+			return
+		elif prefabFullPath != null:
+			var objectInstance:Node3D = load(pathToPrefab).instantiate()
+			objectInstance.rotation = objectRotation
+			objectInstance.position = objectPosition
+			
+			add_child(objectInstance)
+
 func add_cherry_type(newType:String) -> void:
 	cherryTypes.append(newType)
 
@@ -59,8 +75,23 @@ func get_cherry_count() -> int:
 	var currentCherries:int = cherriesContainer.get_child_count(true)
 	return currentCherries
 
-func get_current_hovered_item():
-	pass
+func get_current_hovered_item() -> Node3D:
+	if playerRaycast.is_colliding():
+		var theObject = playerRaycast.get_collider()
+		var theObjectsParent:Node3D = theObject.get_parent()
+		return theObjectsParent
+	return
+
+func set_hovered_to_held_item():
+	if !isHoldingItem:
+		var object:Node3D = get_current_hovered_item()
+		if playerRaycast.is_colliding():
+			if get_current_hovered_item():
+				if object.is_in_group("cherry"):
+					currentHeldItem = object
+					isHoldingItem = true
+	else :
+		print("Could not pick item up: Already have one in hand.")
 
 func is_section_clear() -> bool:
 	var cherryCount:int = cherriesContainer.get_child_count(true)
@@ -89,18 +120,26 @@ func _ready() -> void:
 	_new_cherry_section()
 	_create_cherry_on_bush(cherryTypes[1], Vector3(0,0,0.3), Vector3(0,0,0), 1)
 	await get_tree().create_timer(1.5).timeout
+	#currentHeldItem = $cherries/CherryOnBush2
+	isHoldingCherry = true
 	await get_tree().create_timer(8.5).timeout
+	isHoldingCherry = true
 
 func _input(event: InputEvent) -> void:
-	pass
+	if Input.is_action_just_pressed("interact"):
+		set_hovered_to_held_item()
+
+func _physics_process(delta: float) -> void:
+	# Player Stuff
+	if isHoldingItem && currentHeldItem != null:
+		if isHoldingCherry == true && currentHeldItem != null:
+			currentHeldItem.rotation.y = playerItemHolderHand.global_rotation.y
+			currentHeldItem.rotation.x = playerItemHolderHand.global_rotation.x
+			currentHeldItem.position = lerp(Vector3(currentHeldItem.position.x, currentHeldItem.position.y, currentHeldItem.position.z), Vector3(playerItemHolderHand.global_position.x, playerItemHolderHand.global_position.y, playerItemHolderHand.global_position.z), 5 * delta)
 
 func _process(delta: float) -> void:
 	# Player Stuff
-	if isHoldingCherry == true:
-		currentHeldCherry.rotation.y = playerItemHolderHand.global_rotation.y
-		currentHeldCherry.rotation.x = playerItemHolderHand.global_rotation.x
-		currentHeldCherry.position = lerp(Vector3(currentHeldCherry.position.x, currentHeldCherry.position.y, currentHeldCherry.position.z), Vector3(playerItemHolderHand.global_position.x, playerItemHolderHand.global_position.y, playerItemHolderHand.global_position.z), 5 * delta)
-
+	currentHoveredItem = get_current_hovered_item()
 	# Cherry Handler
 	if is_section_clear():
 		pass
