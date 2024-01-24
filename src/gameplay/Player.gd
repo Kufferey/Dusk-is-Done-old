f963f6793a429ec80aa0b1888a684d72bca91d10
Kufferey <109652222+Kufferey@users.noreply.github.com>
2024-01-23 20:24:57 -0500

    Revert "Basics."

--- src/gameplay/Player.gd
+++
@@ -1,9 +0,0 @@
-extends Node3D
-
-@onready var camera:Node3D = $cameraPos
-
-func _input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
-		rotate_y(-deg_to_rad(event.relative.x / 2))
-		camera.rotate_x(-deg_to_rad(event.relative.y  / 2))
-		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-85), deg_to_rad(85))
